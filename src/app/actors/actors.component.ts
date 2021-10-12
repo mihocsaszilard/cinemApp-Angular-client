087fb817bf4888bfaa4f9dfadfc8d6f924e86c7f
Mihocsa Szilard <mihocsa48@gmail.com>
2021-10-12 21:42:13 +0200

    implemented actors component

--- src/app/actors/actors.component.ts
+++ src/app/actors/actors.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FetchApiDataService } from '../fetch-api-data.service';
 
 @Component({
   selector: 'app-actors',
@@ -7,9 +8,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ActorsComponent implements OnInit {
 
-  constructor() { }
+  actors: any[] = [];
+
+  constructor(public fetchApiData: FetchApiDataService) { }
 
   ngOnInit(): void {
+    this.getActors();
   }
 
+  getActors(): void {
+    this.fetchApiData.getActors().subscribe((response: any) => {
+      this.actors = response;
+      console.log(this.actors);
+      return this.actors;
+    });
+  }
 }
