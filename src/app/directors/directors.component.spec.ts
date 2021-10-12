d8fbcf68eca60689a53fa877c91110dfb3f7080c
Mihocsa Szilard <mihocsa48@gmail.com>
2021-10-12 19:26:10 +0200

    created directors component

---
+++ src/app/directors/directors.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { DirectorsComponent } from './directors.component';
+
+describe('DirectorsComponent', () => {
+  let component: DirectorsComponent;
+  let fixture: ComponentFixture<DirectorsComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ DirectorsComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(DirectorsComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
