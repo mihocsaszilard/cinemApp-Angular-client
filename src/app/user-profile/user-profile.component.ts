d2927a947e1809028c43d7bcd7016b7cde0e6746
Mihocsa Szilard <mihocsa48@gmail.com>
2021-10-13 15:04:03 +0200

    created user profile

--- src/app/user-profile/user-profile.component.ts
+++ src/app/user-profile/user-profile.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FetchApiDataService } from '../fetch-api-data.service';
 
 @Component({
   selector: 'app-user-profile',
@@ -7,9 +8,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UserProfileComponent implements OnInit {
 
-  constructor() { }
+  constructor(public fetchApiData: FetchApiDataService) { }
+
+  user: any = {};
 
   ngOnInit(): void {
+    this.getUserData();
   }
 
+  getUserData(): void {
+    const currentUser = localStorage.getItem('user');
+    this.fetchApiData.getUser(currentUser).subscribe((response: any) => {
+      this.user = response;
+      console.log(this.user);
+      return this.user;
+    });
+  }
 }
+
