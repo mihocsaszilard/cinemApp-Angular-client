79ee42b10b3f9bc919fc30983cd0d14636b6c6a2
Mihocsa Szilard <mihocsa48@gmail.com>
2021-10-14 16:32:46 +0200

    solved genre modal description issue

--- src/app/genre-modal/genre-modal.component.ts
+++ src/app/genre-modal/genre-modal.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, Inject } from '@angular/core';
 import { MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { FetchApiDataService } from '../fetch-api-data.service';
 
 @Component({
   selector: 'app-genre-modal',
@@ -8,14 +9,25 @@ import { MAT_DIALOG_DATA } from '@angular/material/dialog';
 })
 export class GenreModalComponent implements OnInit {
 
+  genres: any[] = [];
+
   constructor(
+    public fetchApiData: FetchApiDataService,
     @Inject(MAT_DIALOG_DATA)
     public data: {
       Name: string;
-      Description: string;
     }
   ) { }
 
   ngOnInit(): void {
+    this.getGenreDescription();
+  }
+
+  getGenreDescription(): void {
+    this.fetchApiData.getGenres().subscribe((response: any) => {
+      this.genres = response;
+      console.log(this.genres);
+      return this.genres;
+    });
   }
 }
