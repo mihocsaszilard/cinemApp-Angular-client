f6ae8e70802d91308c7af64f539a5c588cbd0817
Mihocsa Szilard <mihocsa48@gmail.com>
2021-10-14 17:43:26 +0200

    added favorite movies to profile

--- src/app/user-profile/user-profile.component.ts
+++ src/app/user-profile/user-profile.component.ts
@@ -11,18 +11,48 @@ export class UserProfileComponent implements OnInit {
   constructor(public fetchApiData: FetchApiDataService) { }
 
   user: any = {};
+  movies: any[] = [];
+  favorites: any[] = [];
+  showFavs: any[] = [];
+  favoriteMovies: any = [];
 
   ngOnInit(): void {
     this.getUserData();
+    this.getFavorites();
   }
 
   getUserData(): void {
     const currentUser = localStorage.getItem('user');
     this.fetchApiData.getUser(currentUser).subscribe((response: any) => {
       this.user = response;
-      console.log(this.user);
-      return this.user;
+      this.getMovies();
     });
+  }
+
+  getFavorites(): void {
+    this.fetchApiData.getUser(localStorage.getItem('user')).subscribe((resp: any) => {
+      this.favorites = resp.FavoriteMovies;
+      // console.log('favs', this.favorites);
+      return this.favorites;
+    });
+  }
+
+  getMovies(): void {
+    this.fetchApiData.getAllMovies().subscribe((resp: any) => {
+      this.showFavs = resp;
+      // console.log(this.showFavs);
+      return this.filterMovies();
+    });
+  }
+
+  filterMovies(): void {
+    this.showFavs.forEach((movie: any) => {
+      if (this.favorites.includes(movie._id)) {
+        this.favoriteMovies.push(movie);
+      }
+      // console.log(this.favoriteMovies);
+    });
+    return this.favoriteMovies;
   }
 }
 
